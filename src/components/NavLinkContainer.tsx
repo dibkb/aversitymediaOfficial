0a9ef403565432268575e006fce4339ebe88c74f
dibkb <dibas9110@gmail.con>
2022-03-07 13:52:29 +0530

    responsive home page

--- src/components/NavLinkContainer.tsx
+++ src/components/NavLinkContainer.tsx
@@ -4,62 +4,33 @@ import Link from "next/link";
 import styles from "../../styles/Navbar.module.scss";
 import { Tick } from "../../public/assets/Icons";
 import { NavLinkContainerProps } from "../../types/Types";
+import { navLinks } from "./navLinkInfo";
 const NavLinkContainer: React.FC<NavLinkContainerProps> = ({
   setShowNav,
   pageUrl,
 }) => {
   return (
     <>
-      <Link href="/" passHref>
-        <div className={`${pageUrl === "" ? styles["active"] : null}`}>
-          <a onClick={() => setShowNav(false)}>Home</a>
-          <span className={styles["tick"]}>
-            <Tick />
-          </span>
-        </div>
-      </Link>
-      <Link href="/getstarted" passHref>
-        <div
-          className={`${pageUrl === "getstarted" ? styles["active"] : null}`}
-        >
-          <a onClick={() => setShowNav(false)}>Get Started</a>
-          <span className={styles["tick"]}>
-            <Tick />
-          </span>
-        </div>
-      </Link>
-      <Link href="/services" passHref>
-        <div className={`${pageUrl === "services" ? styles["active"] : null}`}>
-          <a onClick={() => setShowNav(false)}>Our Services</a>
-          <span className={styles["tick"]}>
-            <Tick />
-          </span>
-        </div>
-      </Link>
-      <Link href="/work" passHref>
-        <div className={`${pageUrl === "work" ? styles["active"] : null}`}>
-          <a onClick={() => setShowNav(false)}>Our Work</a>
-          <span className={styles["tick"]}>
-            <Tick />
-          </span>
-        </div>
-      </Link>
-      <Link href="/workflow" passHref>
-        <div className={`${pageUrl === "workflow" ? styles["active"] : null}`}>
-          <a onClick={() => setShowNav(false)}>Our WorkFlow</a>
-          <span className={styles["tick"]}>
-            <Tick />
-          </span>
-        </div>
-      </Link>
-      <Link href="/contact" passHref>
-        <div className={`${pageUrl === "contact" ? styles["active"] : null}`}>
-          <a onClick={() => setShowNav(false)}>Contact Us</a>
-          <span className={styles["tick"]}>
-            <Tick />
-          </span>
-        </div>
-      </Link>
+      {navLinks.map((nav, index) => {
+        return (
+          <Link
+            key={nav.urlEndPoint + index}
+            href={`/${nav.urlEndPoint}`}
+            passHref
+          >
+            <div
+              className={`${
+                pageUrl === nav.urlEndPoint ? styles["active"] : null
+              }`}
+            >
+              <a onClick={() => setShowNav(false)}>{nav.text}</a>
+              <span className={styles["tick"]}>
+                <Tick />
+              </span>
+            </div>
+          </Link>
+        );
+      })}
     </>
   );
 };
---
+++ src/components/navLinkInfo.ts
@@ -0,0 +1,26 @@
+export const navLinks = [
+  {
+    urlEndPoint: "",
+    text: "Home",
+  },
+  {
+    urlEndPoint: "getstarted",
+    text: "Get Started",
+  },
+  {
+    urlEndPoint: "services",
+    text: "Our Services",
+  },
+  {
+    urlEndPoint: "work",
+    text: "Our Work",
+  },
+  {
+    urlEndPoint: "workflow",
+    text: "Our WorkFlow",
+  },
+  {
+    urlEndPoint: "contact",
+    text: "Contact Us",
+  },
+];
