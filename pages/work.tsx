92ef9c654e8d22fca1604ce79fc0d95181a98682
dibkb <dibas9110@gmail.con>
2022-04-02 20:29:24 +0530

    under construnction disclamair

--- pages/work.tsx
+++ pages/work.tsx
@@ -1,6 +1,11 @@
 import React from "react";
-
+import styles from "../styles/work.module.scss";
 const Portfolio: React.FC = () => {
-  return <>Work</>;
+  return (
+    <div className={styles["work__container"]}>
+      <h1>Comming Soon ...</h1>
+      <p>The site is under construction. This page will be added shortly.</p>
+    </div>
+  );
 };
 export default Portfolio;
