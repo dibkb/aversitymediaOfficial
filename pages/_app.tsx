d86b5a1e0d970e4c51f5525ea46b5ba389ce57b6
dibkb <dibas9110@gmail.con>
2022-04-02 21:32:49 +0530

    head tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,14 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import Layout from "../src/components/Layout";
 import "./styles.css";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <Head>
+        <title>AVERSITY Media</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
